a5fe438b9773e7a9a4957e5549a660b7379e2260
PhileasG404 <phileas.G404@gmail.com>
2022-12-10 11:32:45 +0100

    Mon_Tp_Youtube

--- tp.js
+++ tp.js
@@ -1,17 +1,43 @@
-var form = document.querySelector("form");
-var log = document.querySelector("#log");
-let images = document.querySelectorAll("#logo_YT");
+var change_color = document.querySelector(".change_color");
+var colors_h10 = document.querySelectorAll("h10");
+var iframes1 = document.querySelectorAll("iframe");
+var image = document.querySelector(".image");
+var color = document.querySelector(".color");
+var hr_colors = document.querySelectorAll("hr")
 
-form.addEventListener("submit", function(event) {
-    var data = new FormData(form);
-    var output = "";
-    for (const entry of data) {
-      output = entry[0] + " = " + entry[1] + "\r";
-    };
-    if (output == "couleur = Noir") {
-        image.src= image.src.replace('_2.jpg', '.jpg');
-    } else {
-        
+change_color.addEventListener("click", (e) => {
+
+    document.querySelector("body").classList.toggle("darkmode2");
+    document.querySelector(".color").classList.toggle("darkmode");
+
+    colors_h10.forEach(color=>{
+        color.classList.toggle("darkmode");
+    });
+    hr_colors.forEach(hr_color=>{
+        hr_color.classList.toggle("darkmode");
+    });
+})
+
+function save(){
+  let iframe = document.getElementById("iframeCode").value
+  window.localStorage.setItem("iframe" + (+new Date()), iframe);
+  window.location.reload()
+};
+
+window.onload = function load() {
+    let iframes = {...localStorage};
+    let html = "";
+
+    for([key, iframe] of Object.entries(iframes)) {
+        html += iframe + `<button class="delete" style="position:relative;left:-30px;top:-10px;" data-key="${key}">X</button>`
     }
-    event.preventDefault();
-  }, false);
+
+    document.querySelector('.video-play').innerHTML = html;
+
+    document.querySelectorAll('button.delete').forEach((button) => {
+        button.addEventListener("click", () => {
+            window.localStorage.removeItem(button.dataset.key);
+            window.location.reload();
+        })
+    })
+}
